4df24b0ca51a2941a0ee6ee7ddc5978fe3ad50a4
DaniloVrancic <danilov98@hotmail.com>
2025-09-05 23:00:12 +0200

    NS 0.1: Initial file management and configs

---
+++ backend/nail-salon-backend/src/main/java/org/etfbl/nailsalonbackend/NailSalonBackendApplication.java
@@ -0,0 +1,13 @@
+package org.etfbl.nailsalonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailSalonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NailSalonBackendApplication.class, args);
+    }
+
+}
---
+++ backend/nail-salon-backend/src/test/java/org/etfbl/nailsalonbackend/NailSalonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etfbl.nailsalonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailSalonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
